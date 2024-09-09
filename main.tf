f6e254327d0f81b0344b6851b6f0978ba7772978
Romain Deville <code@romaindeville.fr>
2024-09-10 00:28:08 +0200

    ✨ Add repository ruleset deployment

--- main.tf
+++ main.tf
@@ -55,3 +55,131 @@ resource "github_repository" "this" {
     }
   }
 }
+
+# Manage ruletsets of an organization.
+resource "github_repository_ruleset" "this" {
+  for_each = var.ruleset
+
+  repository = github_repository.this.name
+
+  name        = each.key
+  target      = each.value.target
+  enforcement = each.value.enforcement
+
+  dynamic "bypass_actors" {
+    for_each = each.value.bypass_actors != null ? each.value.bypass_actors : []
+
+    content {
+      actor_id    = bypass_actors.value.actor_id
+      actor_type  = bypass_actors.value.actor_type
+      bypass_mode = bypass_actors.value.bypass_mode
+    }
+  }
+
+  dynamic "conditions" {
+    for_each = each.value.conditions != null ? [each.value.conditions] : []
+
+    content {
+      ref_name {
+        include = conditions.value.ref_name.include
+        exclude = conditions.value.ref_name.exclude
+      }
+    }
+  }
+
+  rules {
+    creation                = each.value.rules.creation
+    update                  = each.value.rules.update
+    deletion                = each.value.rules.deletion
+    non_fast_forward        = each.value.rules.non_fast_forward
+    required_linear_history = each.value.rules.required_linear_history
+    required_signatures     = each.value.rules.required_signatures
+
+    dynamic "required_deployments" {
+      for_each = each.value.rules.required_deployments != null ? [each.value.rules.required_deployments] : []
+
+      content {
+        required_deployment_environments = required_deployments.value
+      }
+    }
+
+    dynamic "branch_name_pattern" {
+      for_each = each.value.target == "branch" && each.value.rules.name_pattern != null ? [each.value.rules.name_pattern] : []
+
+      content {
+        name     = branch_name_pattern.value.name
+        negate   = branch_name_pattern.value.negate
+        operator = branch_name_pattern.value.operator
+        pattern  = branch_name_pattern.value.pattern
+      }
+    }
+
+    dynamic "tag_name_pattern" {
+      for_each = each.value.target == "tag" && each.value.rules.name_pattern != null ? [each.value.rules.name_pattern] : []
+
+      content {
+        name     = tag_name_pattern.value.name
+        negate   = tag_name_pattern.value.negate
+        operator = tag_name_pattern.value.operator
+        pattern  = tag_name_pattern.value.pattern
+      }
+    }
+
+    dynamic "commit_author_email_pattern" {
+      for_each = each.value.rules.commit_author_email_pattern != null ? [each.value.rules.commit_author_email_pattern] : []
+
+      content {
+        name     = commit_author_email_pattern.value.name
+        operator = commit_author_email_pattern.value.operator
+        pattern  = commit_author_email_pattern.value.pattern
+        negate   = commit_author_email_pattern.value.negate
+      }
+    }
+
+    dynamic "committer_email_pattern" {
+      for_each = each.value.rules.committer_email_pattern != null ? [each.value.rules.committer_email_pattern] : []
+
+      content {
+        name     = committer_email_pattern.value.name
+        operator = committer_email_pattern.value.operator
+        pattern  = committer_email_pattern.value.pattern
+        negate   = committer_email_pattern.value.negate
+      }
+    }
+
+    dynamic "commit_message_pattern" {
+      for_each = each.value.rules.commit_message_pattern != null ? [each.value.rules.commit_message_pattern] : []
+
+      content {
+        name     = commit_message_pattern.value.name
+        operator = commit_message_pattern.value.operator
+        pattern  = commit_message_pattern.value.pattern
+        negate   = commit_message_pattern.value.negate
+      }
+    }
+
+    dynamic "pull_request" {
+      for_each = each.value.rules.pull_request != null ? [each.value.rules.pull_request] : []
+
+      content {
+        dismiss_stale_reviews_on_push     = pull_request.value.dismiss_stale_reviews_on_push
+        require_code_owner_review         = pull_request.value.require_code_owner_review
+        require_last_push_approval        = pull_request.value.require_last_push_approval
+        required_approving_review_count   = pull_request.value.required_approving_review_count
+        required_review_thread_resolution = pull_request.value.required_review_thread_resolution
+      }
+    }
+
+    dynamic "required_status_checks" {
+      for_each = each.value.rules.required_status_checks != null ? [each.value.rules.required_status_checks] : []
+
+      content {
+        required_check {
+          context        = required_status_checks.value.context
+          integration_id = required_status_checks.value.integration_id
+        }
+        strict_required_status_checks_policy = required_status_checks.value.strict_required_status_checks_policy
+      }
+    }
+  }
+}
